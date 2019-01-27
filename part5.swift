5bce34f22d134aa12178497c9972dfc40eb3e3ee
雨夜 <kiras0518@gmail.com>
2019-01-27 16:54:33 +0800

    Create part5.swift

---
+++ part5.swift
@@ -0,0 +1,15 @@
+import Foundation
+import UIKit
+
+//value: Any, value: [String: Int]
+//var profile: [String: Int] = ["Bob": 97, "Tom": 18, "Ani": 28]
+//var name = [String](profile.keys)
+//var age = [Int](profile.values)
+//var addresses: [String: Int] = [:]
+
+var profile:Dictionary <String, Any> = [
+    "name": [String](),
+    "age": [Int](),
+    "addresses": ["address": [String](),"city": [String](), "zip": [Int]()]
+    ] 
+
